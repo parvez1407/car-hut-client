a6d919319fb976e0da05ba5e33aacf178cde42fe
parvez1407 <info.nahidwd@gmail.com>
2022-11-25 19:08:34 +0600

    impliment login with email and pass and google login and added a spiner in login button

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,13 +1,17 @@
 import React, { useContext, useState } from 'react';
-import { Link, } from 'react-router-dom';
+import { Link, useLocation, useNavigate, } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import loginImage from '../../../assets/login.png';
 import { AuthContext } from '../../../context/AuthProvider';
+import SmallSpinner from '../../../context/Loader/SmallSpinner';
 
 
 const Login = () => {
-    const { signIn, googleProviderLogin } = useContext(AuthContext);
+    const { signIn, googleProviderLogin, loading, setLoading } = useContext(AuthContext);
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
 
 
     // signIn with email and password
@@ -40,9 +44,36 @@ const Login = () => {
         }
         // console.log(email, password);
 
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                toast.success('User Login Successfully', { autoClose: 500 })
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.error(error.message)
+                const err = error.message;
+                toast.error(err, { autoClose: 500 })
+                setLoading(false)
+            })
+
     }
 
-
+    // signIn with google 
+    const handleSignInWithGoogle = () => {
+        googleProviderLogin()
+            .then(result => {
+                const user = result.user;
+                toast.success('User Register Successfully', { autoClose: 500 })
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.error(error)
+                const errorMessage = error.message;
+                toast.error(errorMessage, { autoClose: 500 });
+            })
+    }
 
     return (
         <div className="sm:px-0 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -51,7 +82,7 @@ const Login = () => {
                     <img src={loginImage} alt="" className='w-full' />
                 </div>
                 <div className="lg:w-1/2">
-                    <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100 mt-10">
+                    <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-indigo-900 dark:text-gray-100 mt-10">
                         <h1 className="text-3xl font-bold text-center">Login</h1>
                         <p className='text-red-500 my-3 text-center'>{error}</p>
                         <form onSubmit={handleLogIn} className="space-y-6 ng-untouched ng-pristine ng-valid">
@@ -66,7 +97,11 @@ const Login = () => {
                                     <a rel="noopener noreferrer" href="/">Forgot Password?</a>
                                 </div>
                             </div>
-                            <button className="block w-full p-3 text-center rounded-sm dark:text-gray-200 bg-indigo-600 hover:bg-indigo-500">Sign in</button>
+                            <div>
+                                <button className="block w-full p-3 text-center rounded-md dark:text-gray-200 bg-gradient-to-r from-indigo-600 to-purple-600 hover:to-indigo-600 hover:from-purple-600">
+                                    {loading ? <SmallSpinner /> : 'Sign in'}
+                                </button>
+                            </div>
                         </form>
                         <div className="flex items-center pt-4 space-x-1">
                             <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
@@ -74,7 +109,7 @@ const Login = () => {
                             <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
                         </div>
                         <div className="flex justify-center space-x-4">
-                            <button aria-label="Log in with Google" className="p-3 rounded-sm">
+                            <button onClick={handleSignInWithGoogle} aria-label="Log in with Google" className="p-3 rounded-sm">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-7 h-7 fill-current text-gray-200 hover:text-indigo-400">
                                     <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                                 </svg>
